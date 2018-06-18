ece890bb4bb3d92c176fa4f7d981886487b3bb69
Tomislav Hlevnjak <hlevnjak.tomislav@gmail.com>
2018-06-18 20:11:30 +0200

    First commit

---
+++ src/main/java/foi/hr/vasweb/VaswebApplication.java
@@ -0,0 +1,12 @@
+package foi.hr.vasweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaswebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaswebApplication.class, args);
+	}
+}
---
+++ src/test/java/foi/hr/vasweb/VaswebApplicationTests.java
@@ -0,0 +1,16 @@
+package foi.hr.vasweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VaswebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
